0282efcc758ed79911c5d9907934aa6b87b8388c
hashimrmzn <hashimramzan246@gmail.com>
2025-12-04 05:48:30 -0500

    authroutes done

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');
+
+// Step 1 - Task 3: Create Pino logger instance
+const logger = pino();
+
+dotenv.config();
+
+// Step 1 - Task 4: Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+        // Task 1: Connect to MongoDB through connectToDatabase
+        const db = await connectToDatabase();
+
+        // Task 2: Access the users collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for existing email
+        const existingEmail = await collection.findOne({ email: req.body.email });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+
+        // Hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+
+        // Task 4: Save user details in DB
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        // Task 5: Create JWT token
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        logger.info('User registered successfully');
+
+        res.json({ authtoken, email: req.body.email });
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
